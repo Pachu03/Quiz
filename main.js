ce8fc7f37e8d8e99a92bcfdc562eaaa2a240cd32
Tu Nombre <tu@ejemplo.com>
2024-10-10 09:29:30 +0200

    estilos insertados en la web

--- main.js
+++ main.js
@@ -7,9 +7,11 @@ const TEXTBUTTOM = ["London", "Berlin", "Paris", "Madrid", "Previous", "Next"];
 let btns = [];
 
 let divContainer = document.createElement("div");
+divContainer.className="container";
 let h2 = document.createElement("h2");
 let p = document.createElement("p");
 let ul = document.createElement("ul");
+ul.className="container-answers";
 let li = document.createElement("li");
 let btn = document.createElement("input");
 
@@ -21,24 +23,28 @@ btn.textContent = TEXTBUTTOM[0];
 
 for (let i = 0; i < 4; i++) {
   let li = document.createElement("li");
+  li.className="answer";
   let btn = document.createElement("input");
   btn.type = "button";
   btn.value = TEXTBUTTOM[i];
+  btn.className="answer-btn"
   btns.push(btn);
   li.append(btn);
   ul.append(li);
 }
 
 let div = document.createElement("div");
+div.className="container-footer"
 
 let prevBtn = document.createElement("input");
 prevBtn.type = "button";
 prevBtn.value = TEXTBUTTOM[4];
+prevBtn.className="footer-btn"
 
 let nextBtn = document.createElement("input");
 nextBtn.type = "button";
 nextBtn.value = TEXTBUTTOM[5];
-
+nextBtn.className="footer-btn";
 div.append(prevBtn, nextBtn);
 
 divContainer.append(h2, p, ul, div);
