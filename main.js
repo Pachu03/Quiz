6176b35871b8d48351c4aeb645f5ecee96cae6be
Tu Nombre <tu@ejemplo.com>
2024-10-10 09:20:55 +0200

    estructura del js y agregar estilos

--- main.js
+++ main.js
@@ -1,2 +1,46 @@
-import './style.css'
-const body = document.body;
+const BODY = document.body;
+
+const TEXTELEMENT = ["Quiz Question", "What is the capital of France?"];
+
+const TEXTBUTTOM = ["London", "Berlin", "Paris", "Madrid", "Previous", "Next"];
+
+let btns = [];
+
+let divContainer = document.createElement("div");
+let h2 = document.createElement("h2");
+let p = document.createElement("p");
+let ul = document.createElement("ul");
+let li = document.createElement("li");
+let btn = document.createElement("input");
+
+h2.textContent = TEXTELEMENT[0];
+p.textContent = TEXTELEMENT[1];
+
+btn.type = "button";
+btn.textContent = TEXTBUTTOM[0];
+
+for (let i = 0; i < 4; i++) {
+  let li = document.createElement("li");
+  let btn = document.createElement("input");
+  btn.type = "button";
+  btn.value = TEXTBUTTOM[i];
+  btns.push(btn);
+  li.append(btn);
+  ul.append(li);
+}
+
+let div = document.createElement("div");
+
+let prevBtn = document.createElement("input");
+prevBtn.type = "button";
+prevBtn.value = TEXTBUTTOM[4];
+
+let nextBtn = document.createElement("input");
+nextBtn.type = "button";
+nextBtn.value = TEXTBUTTOM[5];
+
+div.append(prevBtn, nextBtn);
+
+divContainer.append(h2, p, ul, div);
+
+BODY.append(divContainer);
